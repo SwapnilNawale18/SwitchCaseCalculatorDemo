7a916d6ed65f422c30702fdbbee22a83a0bb9836
Swapnil Nawale <swapnilnawale18@gmail.com>
2021-08-28 19:10:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SwitchCaseCalculatorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c;
+            char d;
+            Console.Write("Enter first operand: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second operand: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter operator: ");
+            d = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Operation: ");
+            switch (d)
+            {
+                case '+':
+                    c = a + b;
+                    Console.WriteLine("{0} + {1} = {2}", a, b, c);
+                    break;
+                case '-':
+                    c = a - b;
+                    Console.WriteLine("{0} - {1} = {2}", a, b, c);
+                    break;
+                case '*':
+                    c = a * b;
+                    Console.WriteLine("{0} * {1} = {2}", a, b, c);
+                    break;
+                case '/':
+                    c = a / b;
+                    Console.WriteLine("{0} / {1} = {2}", a, b, c);
+                    break;
+                case '%':
+                    c = a % b;
+                    Console.WriteLine("{0} % {1} = {2}", a, b, c);
+                    break;
+                default:
+                    Console.WriteLine("Invalid");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
